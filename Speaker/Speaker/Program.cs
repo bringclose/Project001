19b94d7944571d7e48209ccf81654c84d03be948
bringclose <spam1703@gmail.com>
2013-10-09 17:22:19 +0700

    + Init project & adding show console

---
+++ Speaker/Speaker/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+//Adding Console
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+//Adding Console CB link: http://social.msdn.microsoft.com/Forums/en-US/b7a14400-6d72-4fbf-9927-0966f69ef4a2/how-to-open-console-window-in-windows-apllication?forum=csharplanguage
+
+namespace Speaker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        //Adding Console
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int MY_CODE_PAGE = 437;
+        //Adding Console CB
+        static void Main()
+        {
+            //Adding Console
+            Console.WriteLine("This text you can see in debug output window.");
+
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = System.Text.Encoding.GetEncoding(MY_CODE_PAGE);
+            StreamWriter standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+
+            Console.WriteLine("This text you can see in console window.");
+            //Adding Console CB
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Speaker/Speaker/src/test.cs
@@ -0,0 +1,14 @@
+﻿
+//ReadFromFile
+namespace Speaker
+{
+    class ReadFromFile
+    {
+        static string dataFile = @".\..\..\res\data.dat";
+        public static void ReadingText()
+        {
+            string text = System.IO.File.ReadAllText(dataFile);
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+        }
+    }
+}
