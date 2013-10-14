bf5923752be1baee043fd607e2287d7b8b70ca14
bringclose <spam1703@gmail.com>
2013-10-14 21:20:01 +0700

    + Add ClearSpace function.

--- Speaker/Speaker/Form1.cs
+++ Speaker/Speaker/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Speaker1;
 using Speaker.src;
 
+
 namespace Speaker
 {
     public partial class Form1 : Form
@@ -22,16 +23,12 @@ namespace Speaker
         private void button1_Click(object sender, EventArgs e)
         {
             string stmp = Speaker.src.Utils.sReadingAllText();
-            _lblText2.Text = stmp;
             System.Console.WriteLine("Contents of WriteText.txt = {0}", stmp);
-            _lblText.Text = Speaker.src.Text.sClearAllSpace(stmp);
+            _lblText.Text = stmp;
             System.Console.WriteLine("Contents of WriteText.txt = {0}", _lblText.Text);
             string stmp1 = Speaker.src.Text.sClearSpace(stmp);
+            _lblText2.Text = stmp1;
             System.Console.WriteLine("Contents of WriteText.txt = {0}", stmp1);
-
-            string abc = " Tran      ngoc Duy    ";
-            abc.Trim();
-            System.Console.WriteLine("Contents of WriteText.txt = {0}", abc);
         }
     }
 }
--- Speaker/Speaker/src/Text.cs
+++ Speaker/Speaker/src/Text.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Speaker.src
 {
@@ -15,8 +16,41 @@ namespace Speaker.src
 
         public static string sClearSpace(string sText)
         {
-            string sTmp = sText.Trim();
-            return sTmp;
+            return Regex.Replace(sText, @"\s+", " ").Trim();
+        }
+
+        public static string TrimWhitespaceAnd(string inputString, params char[] characters)
+        {
+            int start = 0;
+            while (start < inputString.Length)
+            {
+                if (!char.IsWhiteSpace(inputString[start]) &&
+                        !characters.Contains(inputString[start]))
+                {
+                    break;
+                }
+                start++;
+            }
+            int end = inputString.Length - 1;
+            while (end >= start)
+            {
+                if (!char.IsWhiteSpace(inputString[end]) &&
+                        !characters.Contains(inputString[end]))
+                {
+                    break;
+                }
+                end--;
+            }
+            int length = (end - start) + 1;
+            if (length == inputString.Length)
+            {
+                return inputString;
+            }
+            if (length == 0)
+            {
+                return string.Empty;
+            }
+            return inputString.Substring(start, length);
         }
     }
 }
